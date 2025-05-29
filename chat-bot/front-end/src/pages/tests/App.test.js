ba2718a899a19ad3d783843b87814c0ae140987a
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2025-05-29 03:49:16 -0700

    file

--- chat-bot/front-end/src/pages/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, cleanup } from '@testing-library/react'
-import App from "../../App"
-
-test('All of Application should load', () => {
-  render(
-        <App/>
-    );
-  expect(App).toBeTruthy();
-})
